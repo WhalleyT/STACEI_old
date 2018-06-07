71e59249d415bc845a46a8aad343ee80e961c5dd
Tom Whalley <whalleyt@cardiff.ac.uk>
2018-06-07 11:57:39 +0100

    fixed untracked files

--- choose_mutants.R
+++
@@ -1,9 +0,0 @@
-setwd("~/PycharmProjects/STACEI")
-
-data <- read.csv("868_TCR_to_pMHC_contacts_clean.txt", sep="\t", row.names = NULL)
-data <- data[data$Donor_ResNum %in% c("CDR3a", "CDR3b"),]
-data <- data[data$Acceptor_Chain_Letter == "peptide",]
-
-possible <- data %>%
-  group_by(Donor_ResNum, Donor_Annotation, Donor_Chain) %>%
-  tally()
--- choose_mutants.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data = pd.read_csv("868_TCR_to_pMHC_contacts_clean.txt", sep="\t")
-print data["Donor_Annotation "]
-cdrs = data[data["Donor_Annotation "].isin(["CDR3a", "CDR3b"])]
-
-print cdrs
